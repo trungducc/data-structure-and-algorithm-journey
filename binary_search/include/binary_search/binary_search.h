cf5156f51339af644e98194e46db465150df014d
trungducc <gnurtduc@gmail.com>
2018-10-15 14:34:48 +0700

    Add binary search implementation (#9)
    * Add files
    
    * Add implementation

---
+++ binary_search/include/binary_search/binary_search.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <iostream>
+
+namespace td {
+
+// Returns either the index of the location in the vector, or -1 if the vector
+// doesn't contain the target.
+template <typename T>
+std::size_t binary_search(const std::vector<T>& vector, const T& target);
+
+// Binary search implementation using recursion.
+template <typename T>
+std::size_t recursion_binary_search(const std::vector<T>& vector,
+                                    const T& target,
+                                    std::size_t lower_bound,
+                                    std::size_t upper_bound);
+
+}  // namespace td
+
+/****************  Binary search implementation ****************/
+namespace td {
+
+template <typename T>
+std::size_t binary_search(const std::vector<T>& vector, const T& target) {
+  long long lower_bound = 0;
+  long long upper_bound = vector.size() - 1;
+  long long mid_point;
+
+  while (lower_bound <= upper_bound) {
+    mid_point = (lower_bound + upper_bound) / 2;
+
+    if (vector[mid_point] == target) {
+      return mid_point;
+    } else if (vector[mid_point] > target) {
+      upper_bound = mid_point - 1;
+    } else {
+      lower_bound = mid_point + 1;
+    }
+  }
+
+  return -1;
+}
+
+template <typename T>
+std::size_t recursion_binary_search(const std::vector<T>& vector,
+                                    const T& target,
+                                    std::size_t lower_bound,
+                                    std::size_t upper_bound) {
+  if (lower_bound > vector.size() - 1 || upper_bound > vector.size() - 1 ||
+      lower_bound < 0 || upper_bound < 0 || lower_bound > upper_bound)
+    return -1;
+
+  long long mid_point = (lower_bound + upper_bound) / 2;
+
+  if (vector[mid_point] == target) {
+    return mid_point;
+  } else if (vector[mid_point] > target) {
+    upper_bound = mid_point - 1;
+  } else {
+    lower_bound = mid_point + 1;
+  }
+
+  return recursion_binary_search(vector, target, lower_bound, upper_bound);
+}
+
+}  // namespace td
