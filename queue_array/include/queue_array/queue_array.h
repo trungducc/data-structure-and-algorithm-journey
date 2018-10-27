8cbb9f6ca03b34d37600d97d19b5b4410067f62e
trungducc <gnurtduc@gmail.com>
2018-10-27 19:21:15 +0700

    Refactor queue array

--- queue_array/include/queue_array/queue_array.h
+++ queue_array/include/queue_array/queue_array.h
@@ -10,7 +10,7 @@ namespace td {
 constexpr std::size_t default_capacity = 16;
 
 // Implemented queue by using array
-template <typename T>
+template <typename DataType>
 class Queue {
  public:
   Queue();
@@ -25,14 +25,14 @@ class Queue {
   bool is_full();
 
   // Add given value to queue. If queue is full, do nothing.
-  void enqueue(const T& value);
+  void enqueue(const DataType& value);
 
   // Remove least recently-added item from queue and return its value.
-  T dequeue();
+  DataType dequeue();
 
  private:
   // Raw array where items are stored.
-  T* items_{nullptr};
+  DataType* items_{nullptr};
 
   // Calling |enqueue| will insert an item to this index.
   std::size_t enqueue_index_{0};
@@ -51,30 +51,30 @@ class Queue {
 /****************  Queue implementation ****************/
 namespace td {
 
-template <typename T>
-Queue<T>::Queue() : Queue(default_capacity) {}
+template <typename DataType>
+Queue<DataType>::Queue() : Queue(default_capacity) {}
 
-template <typename T>
-Queue<T>::Queue(std::size_t capacity)
-    : items_(new T[++capacity]), capacity_(capacity) {}
+template <typename DataType>
+Queue<DataType>::Queue(std::size_t capacity)
+    : items_(new DataType[++capacity]), capacity_(capacity) {}
 
-template <typename T>
-Queue<T>::~Queue() {
+template <typename DataType>
+Queue<DataType>::~Queue() {
   delete[] items_;
 }
 
-template <typename T>
-bool Queue<T>::is_empty() {
+template <typename DataType>
+bool Queue<DataType>::is_empty() {
   return enqueue_index_ == dequeue_index_;
 }
 
-template <typename T>
-bool Queue<T>::is_full() {
+template <typename DataType>
+bool Queue<DataType>::is_full() {
   return (enqueue_index_ + 1) % capacity_ == dequeue_index_;
 }
 
-template <typename T>
-void Queue<T>::enqueue(const T& value) {
+template <typename DataType>
+void Queue<DataType>::enqueue(const DataType& value) {
   if (is_full()) {
     return;
   }
@@ -88,13 +88,13 @@ void Queue<T>::enqueue(const T& value) {
   }
 }
 
-template <typename T>
-T Queue<T>::dequeue() {
+template <typename DataType>
+DataType Queue<DataType>::dequeue() {
   if (is_empty()) {
     utils::validate(0, 0, utils::Action::kRemoved);
   }
 
-  T data = items_[dequeue_index_];
+  DataType data = items_[dequeue_index_];
   items_[dequeue_index_] = 0;
 
   if (dequeue_index_ == capacity_ - 1) {
