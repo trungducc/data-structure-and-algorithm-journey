c71c7c9b4b3a12c38477710388a2e236fd1de997
trungducc <gnurtduc@gmail.com>
2018-11-12 15:42:35 +0700

    Add files

---
+++ priority_queue/include/priority_queue/priority_queue.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "utils/utils.h"
+
+namespace td {
+
+}  // namespace td
+
+/****************  PriorityQueue implementation ****************/
+namespace td {
+
+}  // namespace td
---
+++ priority_queue/test/priority_queue_test.cc
@@ -0,0 +1,8 @@
+#include "priority_queue/priority_queue.h"
+#include "gtest/gtest.h"
+
+namespace {
+using namespace td;
+
+}  // namespace
+
