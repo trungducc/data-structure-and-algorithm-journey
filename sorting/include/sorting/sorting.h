08df9985e2218796100159cef04efb49c037aed3
trungducc <gnurtduc@gmail.com>
2018-12-13 18:48:03 +0700

    Add insertion sort implementation

--- sorting/include/sorting/sorting.h
+++ sorting/include/sorting/sorting.h
@@ -18,8 +18,10 @@ void bubble_sort(std::vector<ItemType>& items, bool ascending = true) {
 template <typename ItemType>
 void selection_sort(std::vector<ItemType>& items, bool ascending = true) {
   std::size_t min_index;
+
   for (std::size_t i = 0; i < items.size(); ++i) {
     min_index = i;
+
     for (std::size_t j = i; j < items.size(); ++j)
       if (ascending == items[j] < items[min_index])
         min_index = j;
@@ -27,5 +29,25 @@ void selection_sort(std::vector<ItemType>& items, bool ascending = true) {
   }
 }
 
+// Insertion sort implementation
+template <typename ItemType>
+void insertion_sort(std::vector<ItemType>& items, bool ascending = true) {
+  std::size_t curr_index;
+  ItemType inserted_item;
+
+  for (std::size_t i = 1; i < items.size(); ++i) {
+    curr_index = i;
+    inserted_item = items[i];
+
+    while (curr_index > 0 &&
+           ascending == items[curr_index - 1] > inserted_item) {
+      items[curr_index] = items[curr_index - 1];
+      --curr_index;
+    }
+    items[curr_index] = inserted_item;
+  }
+}
+
 }  // namespace sorting
 }  // namespace td
+
--- sorting/test/sorting_test.cc
+++ sorting/test/sorting_test.cc
@@ -32,4 +32,18 @@ TEST(SortingTest, SelectionSort) {
   EXPECT_EQ(std::vector<int>(), items);
 }
 
+TEST(SortingTest, InsertionSort) {
+  std::vector<int> items({2, 8, 4, 7, 5, 9, 1, 3, 6});
+  insertion_sort(items);
+  EXPECT_EQ(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 9}), items);
+
+  items = std::vector<int>({2, 8, 4, 7, 5, 9, 1, 3, 6});
+  insertion_sort(items, false);
+  EXPECT_EQ(std::vector<int>({9, 8, 7, 6, 5, 4, 3, 2, 1}), items);
+
+  items = std::vector<int>();
+  insertion_sort(items);
+  EXPECT_EQ(std::vector<int>(), items);
+}
+
 }  // namespace
