a4821f87e93c8e782ba35eb4a08c33282b0ad11b
trungducc <gnurtduc@gmail.com>
2018-12-13 15:01:45 +0700

    Add selection sort implemetation

--- sorting/include/sorting/sorting.h
+++ sorting/include/sorting/sorting.h
@@ -8,10 +8,23 @@ namespace sorting {
 // Bubble sort implementation
 template <typename ItemType>
 void bubble_sort(std::vector<ItemType>& items, bool ascending = true) {
-  for (int i = 0; i < items.size(); ++i)
-    for (int j = 1; j < items.size() - i; ++j)
+  for (std::size_t i = 0; i < items.size(); ++i)
+    for (std::size_t j = 1; j < items.size() - i; ++j)
       if (ascending == items[j - 1] > items[j])
         std::swap(items[j - 1], items[j]);
+}
+
+// Selection sort implementation
+template <typename ItemType>
+void selection_sort(std::vector<ItemType>& items, bool ascending = true) {
+  std::size_t min_index;
+  for (std::size_t i = 0; i < items.size(); ++i) {
+    min_index = i;
+    for (std::size_t j = i; j < items.size(); ++j)
+      if (ascending == items[j] < items[min_index])
+        min_index = j;
+    std::swap(items[i], items[min_index]);
+  }
 }
 
 }  // namespace sorting
--- sorting/test/sorting_test.cc
+++ sorting/test/sorting_test.cc
@@ -18,4 +18,18 @@ TEST(SortingTest, BubleSort) {
   EXPECT_EQ(std::vector<int>(), items);
 }
 
+TEST(SortingTest, SelectionSort) {
+  std::vector<int> items({2, 8, 4, 7, 5, 9, 1, 3, 6});
+  selection_sort(items);
+  EXPECT_EQ(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 9}), items);
+
+  items = std::vector<int>({2, 8, 4, 7, 5, 9, 1, 3, 6});
+  selection_sort(items, false);
+  EXPECT_EQ(std::vector<int>({9, 8, 7, 6, 5, 4, 3, 2, 1}), items);
+
+  items = std::vector<int>();
+  selection_sort(items);
+  EXPECT_EQ(std::vector<int>(), items);
+}
+
 }  // namespace
