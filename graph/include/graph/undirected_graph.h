0f13d19cdafe2975ded878723723fb415f1ce0bf
trungducc <gnurtduc@gmail.com>
2019-01-05 18:44:16 +0700

    Add undirected graph bfs and dfs implementation

--- graph/include/graph/undirected_graph.h
+++ graph/include/graph/undirected_graph.h
@@ -3,7 +3,8 @@
 #include "graph.h"
 
 #include <queue>
-#include <unordered_map>
+#include <stack>
+#include <map>
 #include <unordered_set>
 
 namespace td {
@@ -12,7 +13,10 @@ namespace td {
 template <typename LabelType>
 class UndirectedGraph : public Graph<LabelType> {
  public:
-  ~UndirectedGraph() override = default;
+  UndirectedGraph() = default;
+  UndirectedGraph(std::initializer_list<LabelType>&& labels);
+
+  ~UndirectedGraph() override;
 
   void add_vertex(const LabelType& label) override;
   void add_edge(const LabelType& source, const LabelType& destination) override;
@@ -25,14 +29,23 @@ class UndirectedGraph : public Graph<LabelType> {
   // Representation of a vertex on undirected graph.
   struct Vertex {
     LabelType label;
-    std::unordered_map<LabelType, Vertex> neighbors;
+    std::map<LabelType, Vertex*> neighbors;
 
     Vertex() = default;
     Vertex(const LabelType& l) : label(l) {}
   };
 
+  // If there is a vertex with given label in graph, return true. Otherwise,
+  // return false.
+  bool has_vertex(const LabelType& label);
+
+  // Depth first traversal from vertex with given label recursively.
+  void dfs_recursive_impl(const LabelType& label,
+                          std::vector<LabelType>& result,
+                          std::unordered_set<LabelType>& visited);
+
   // Vertexs stored in graph.
-  std::unordered_map<LabelType, Vertex> vertexs_;
+  std::map<LabelType, Vertex*> vertexs_;
 };
 
 }  // namespace td
@@ -41,46 +54,59 @@ class UndirectedGraph : public Graph<LabelType> {
 namespace td {
 
 template <typename LabelType>
+UndirectedGraph<LabelType>::~UndirectedGraph() {
+  for (auto kv : vertexs_)
+    delete kv.second;
+}
+
+template <typename LabelType>
+UndirectedGraph<LabelType>::UndirectedGraph(
+    std::initializer_list<LabelType>&& il) {
+  for (LabelType label : il)
+    add_vertex(label);
+}
+
+template <typename LabelType>
 void UndirectedGraph<LabelType>::add_vertex(const LabelType& label) {
-  if (vertexs_.find(label) != vertexs_.end())
+  if (has_vertex(label))
     return;
-  vertexs_[label] = Vertex(label);
+  vertexs_[label] = new Vertex(label);
 }
 
 template <typename LabelType>
 void UndirectedGraph<LabelType>::add_edge(const LabelType& source,
                                           const LabelType& destination) {
-  if (vertexs_.find(source) == vertexs_.end())
+  if (!has_vertex(source))
     add_vertex(source);
-  if (vertexs_.find(destination) == vertexs_.end())
+  if (!has_vertex(destination))
     add_vertex(destination);
 
-  vertexs_[source].neighbors[destination] = vertexs_[destination];
-  vertexs_[destination].neighbors[source] = vertexs_[source];
+  vertexs_[source]->neighbors[destination] = vertexs_[destination];
+  vertexs_[destination]->neighbors[source] = vertexs_[source];
 }
 
 template <typename LabelType>
 std::vector<LabelType> UndirectedGraph<LabelType>::bfs(const LabelType& label) {
-  if (vertexs_.find(label) == vertexs_.end())
+  if (!has_vertex(label))
     return {};
 
   std::vector<LabelType> result;
   std::unordered_set<LabelType> visited;
-  std::queue<Vertex> frontier;
-  frontier.push(vertexs_[label]);
+  std::queue<Vertex*> to_visit;
+  to_visit.push(vertexs_[label]);
 
-  while (!frontier.empty()) {
-    Vertex v = frontier.front();
-    frontier.pop();
+  while (!to_visit.empty()) {
+    Vertex* v = to_visit.front();
+    to_visit.pop();
 
-    if (visited.find(v.label) != visited.end())
+    if (visited.find(v->label) != visited.end())
       continue;
 
-    result.push_back(v.label);
-    visited.insert(v.label);
+    result.push_back(v->label);
+    visited.insert(v->label);
 
-    for (auto kv : v.neighbors)
-      frontier.push(kv.second);
+    for (auto kv : v->neighbors)
+      to_visit.push(kv.second);
   }
   return result;
 }
@@ -88,13 +114,59 @@ std::vector<LabelType> UndirectedGraph<LabelType>::bfs(const LabelType& label) {
 template <typename LabelType>
 std::vector<LabelType> UndirectedGraph<LabelType>::dfs_recursive(
     const LabelType& label) {
-  return {label};
+  if (!has_vertex(label))
+    return {};
+  std::vector<LabelType> result;
+  std::unordered_set<LabelType> visited;
+  dfs_recursive_impl(label, result, visited);
+  return result;
 }
 
 template <typename LabelType>
 std::vector<LabelType> UndirectedGraph<LabelType>::dfs_iterative(
     const LabelType& label) {
-  return {label};
+  if (!has_vertex(label))
+    return {};
+
+  std::vector<LabelType> result;
+  std::unordered_set<LabelType> visited;
+  std::stack<Vertex*> to_visit;
+  to_visit.push(vertexs_[label]);
+
+  while (!to_visit.empty()) {
+    Vertex* v = to_visit.top();
+    to_visit.pop();
+
+    if (visited.find(v->label) != visited.end())
+      continue;
+
+    result.push_back(v->label);
+    visited.insert(v->label);
+
+    for (auto iter = v->neighbors.rbegin(); iter != v->neighbors.rend(); ++iter)
+      to_visit.push(iter->second);
+  }
+  return result;
+}
+
+template <typename LabelType>
+bool UndirectedGraph<LabelType>::has_vertex(const LabelType& label) {
+  return vertexs_.find(label) != vertexs_.end();
+}
+
+template <typename LabelType>
+void UndirectedGraph<LabelType>::dfs_recursive_impl(
+    const LabelType& label,
+    std::vector<LabelType>& result,
+    std::unordered_set<LabelType>& visited) {
+  if (visited.find(label) != visited.end())
+    return;
+
+  result.push_back(label);
+  visited.insert(label);
+
+  for (auto kv : vertexs_[label]->neighbors)
+    dfs_recursive_impl(kv.first, result, visited);
 }
 
 }  // namespace td
--- graph/test/undirected_graph_test.cc
+++ graph/test/undirected_graph_test.cc
@@ -7,8 +7,18 @@ using namespace td;
 typedef std::string LabelType;
 typedef UndirectedGraph<LabelType> GraphType;
 
+TEST(UndirectedGraphTest, InitializerListConstructor) {
+  Graph<LabelType>* graph = new GraphType({"D", "B", "C"});
+  EXPECT_EQ(std::vector<LabelType>({"D"}), graph->bfs("D"));
+  EXPECT_EQ(std::vector<LabelType>({"B"}), graph->bfs("B"));
+  EXPECT_EQ(std::vector<LabelType>({"C"}), graph->bfs("C"));
+  EXPECT_EQ(std::vector<LabelType>(), graph->bfs("A"));
+
+  delete graph;
+}
+
 TEST(UndirectedGraphTest, AddVertex) {
-  Graph<LabelType> *graph = new GraphType();
+  Graph<LabelType>* graph = new GraphType();
 
   graph->add_vertex("A");
   EXPECT_EQ(std::vector<LabelType>({"A"}), graph->bfs("A"));
@@ -19,6 +29,104 @@ TEST(UndirectedGraphTest, AddVertex) {
   EXPECT_EQ(std::vector<LabelType>({"A"}), graph->bfs("A"));
   EXPECT_EQ(std::vector<LabelType>({"B"}), graph->bfs("B"));
   EXPECT_EQ(std::vector<LabelType>(), graph->bfs("C"));
+
+  delete graph;
+}
+
+TEST(UndirectedGraphTest, AddEdge) {
+  Graph<LabelType>* graph = new GraphType({"A", "B"});
+  graph->add_edge("A", "B");
+  graph->add_edge("C", "D");
+  graph->add_edge("A", "C");
+  graph->add_edge("C", "B");
+  EXPECT_EQ(std::vector<LabelType>({"A", "B", "C", "D"}), graph->bfs("A"));
+  EXPECT_EQ(std::vector<LabelType>({"B", "A", "C", "D"}), graph->bfs("B"));
+  EXPECT_EQ(std::vector<LabelType>({"C", "A", "B", "D"}), graph->bfs("C"));
+  EXPECT_EQ(std::vector<LabelType>({"D", "C", "A", "B"}), graph->bfs("D"));
+
+  graph->add_edge("A", "E");
+  EXPECT_EQ(std::vector<LabelType>({"A", "B", "C", "E", "D"}), graph->bfs("A"));
+  EXPECT_EQ(std::vector<LabelType>({"D", "C", "A", "B", "E"}), graph->bfs("D"));
+
+  delete graph;
+}
+
+TEST(UndirectedGraphTest, BFS) {
+  Graph<LabelType>* graph = new GraphType({"A", "B", "C", "D", "E", "F"});
+  graph->add_edge("A", "B");
+  graph->add_edge("C", "D");
+  graph->add_edge("A", "C");
+  graph->add_edge("C", "B");
+  graph->add_edge("A", "F");
+  graph->add_edge("B", "F");
+  graph->add_edge("C", "F");
+  graph->add_edge("B", "E");
+  graph->add_edge("D", "E");
+
+  EXPECT_EQ(std::vector<LabelType>({"A", "B", "C", "F", "E", "D"}),
+            graph->bfs("A"));
+  EXPECT_EQ(std::vector<LabelType>({"B", "A", "C", "E", "F", "D"}),
+            graph->bfs("B"));
+  EXPECT_EQ(std::vector<LabelType>({"C", "A", "B", "D", "F", "E"}),
+            graph->bfs("C"));
+  EXPECT_EQ(std::vector<LabelType>({"D", "C", "E", "A", "B", "F"}),
+            graph->bfs("D"));
+  EXPECT_EQ(std::vector<LabelType>({"E", "B", "D", "A", "C", "F"}),
+            graph->bfs("E"));
+  EXPECT_EQ(std::vector<LabelType>({"F", "A", "B", "C", "E", "D"}),
+            graph->bfs("F"));
+
+  delete graph;
+}
+
+TEST(UndirectedGraphTest, DFSRecursive) {
+  Graph<LabelType>* graph = new GraphType({"A", "B", "C", "D", "E", "F"});
+  graph->add_edge("A", "B");
+  graph->add_edge("C", "D");
+  graph->add_edge("C", "B");
+  graph->add_edge("A", "F");
+  graph->add_edge("B", "F");
+  graph->add_edge("D", "E");
+
+  EXPECT_EQ(std::vector<LabelType>({"A", "B", "C", "D", "E", "F"}),
+            graph->dfs_recursive("A"));
+  EXPECT_EQ(std::vector<LabelType>({"B", "A", "F", "C", "D", "E"}),
+            graph->dfs_recursive("B"));
+  EXPECT_EQ(std::vector<LabelType>({"C", "B", "A", "F", "D", "E"}),
+            graph->dfs_recursive("C"));
+  EXPECT_EQ(std::vector<LabelType>({"D", "C", "B", "A", "F", "E"}),
+            graph->dfs_recursive("D"));
+  EXPECT_EQ(std::vector<LabelType>({"E", "D", "C", "B", "A", "F"}),
+            graph->dfs_recursive("E"));
+  EXPECT_EQ(std::vector<LabelType>({"F", "A", "B", "C", "D", "E"}),
+            graph->dfs_recursive("F"));
+
+  delete graph;
+}
+
+TEST(UndirectedGraphTest, DFSIterative) {
+  Graph<LabelType>* graph = new GraphType({"A", "B", "C", "D", "E", "F"});
+  graph->add_edge("A", "B");
+  graph->add_edge("C", "D");
+  graph->add_edge("C", "B");
+  graph->add_edge("A", "F");
+  graph->add_edge("B", "F");
+  graph->add_edge("D", "E");
+
+  EXPECT_EQ(std::vector<LabelType>({"A", "B", "C", "D", "E", "F"}),
+            graph->dfs_iterative("A"));
+  EXPECT_EQ(std::vector<LabelType>({"B", "A", "F", "C", "D", "E"}),
+            graph->dfs_iterative("B"));
+  EXPECT_EQ(std::vector<LabelType>({"C", "B", "A", "F", "D", "E"}),
+            graph->dfs_iterative("C"));
+  EXPECT_EQ(std::vector<LabelType>({"D", "C", "B", "A", "F", "E"}),
+            graph->dfs_iterative("D"));
+  EXPECT_EQ(std::vector<LabelType>({"E", "D", "C", "B", "A", "F"}),
+            graph->dfs_iterative("E"));
+  EXPECT_EQ(std::vector<LabelType>({"F", "A", "B", "C", "D", "E"}),
+            graph->dfs_iterative("F"));
+
+  delete graph;
 }
 
 }  // namespace
